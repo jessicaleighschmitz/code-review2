c64ff7abed035fcf286434208237fba698f22404
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-02-24 11:46:41 -0800

    fix js bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,68 @@
 $(document).ready(function() {
   $('#container form').submit(function(event) {
-
+    event.preventDefault();
+    console.log('hi');
     var answersA = 0;
     var answersB = 0;
     var answersC = 0;
 
-    var queston1 = ('input:radio[name=temperature]:checked]').val();
-    var question2 = ('input:radio[name=activity]:checked').val();
-    var question3 = ('input:radio[name=money]:checked').val();
-    var question4 = ('input:radio[name=transportation]:checked').val();
-    var question5 = ('input:radio[name=wifi]:checked').val();
+    var queston1 = $("input[name='temperature']:checked").val();
+    var question2 = $("input[name='activity']:checked").val();
+    var question3 = $("input[name='money']:checked").val();
+    var question4 = $("input[name='transportation']:checked").val();
+    var question5 = $("input[name='wifi']:checked").val();
 
-    if (question1 === 'a') {
+    if (question1 === "a") {
       answersA += 1;
-    } else if (question1 === 'b') {
+    } else if (question1 === "b") {
       answersB += 1;
-    } else if (question1 === 'c') {
+    } else if (question1 === "c") {
       answersC += 1;
     }
 
-    if (question2 === 'a') {
+    if (question2 === "a") {
       answersA += 1;
-    } else if (question2 === 'b') {
+    } else if (question2 === "b") {
       answersB += 1;
-    } else if (question2 === 'c') {
+    } else if (question2 === "c") {
       answersC += 1;
     }
 
-    if (question3 === 'a') {
+    if (question3 === "a") {
       answersA += 1;
-    } else if (question3 === 'b') {
+    } else if (question3 === "b") {
       answersB += 1;
-    } else if (question3 === 'c') {
+    } else if (question3 === "c") {
       answersC += 1;
     }
 
-    if (question4 === 'a') {
+    if (question4 === "a") {
       answersA += 1;
-    } else if (question4 === 'b') {
+    } else if (question4 === "b") {
       answersB += 1;
-    } else if (question4 === 'c') {
+    } else if (question4 === "c") {
       answersC += 1;
     }
 
-    if (question5 === 'a') {
+    if (question5 === "a") {
       answersA += 1;
-    } else if (question5 === 'b') {
+    } else if (question5 === "b") {
       answersB += 1;
-    } else if (question5 === 'c') {
+    } else if (question5 === "c") {
       answersC += 1;
     }
 
-    if(answersA > 2) {
+    if(answersA >= 3) {
       $('#mexico').show();
-    } else if (answersB > 2) {
+    } else if (answersB >= 3) {
       $('#iceland').show();
-    } else if (answersC > 2) {
+    } else if (answersC >= 3) {
       $('#milan').show();
     } else {
-      $('#unsure').show();
+      $('#subcontent').show();
     }
-    event.preventDefault
+
+
+
   });
 });
